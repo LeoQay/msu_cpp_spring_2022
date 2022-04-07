2eb20130e0f712dad5a9e0a35ebb1f326976079e
LeoQay <reliativism@yandex.ru>
2022-04-08 02:32:48 +0300

    very cool testing

--- 02/test_token_parser.cpp
+++ 02/test_token_parser.cpp
@@ -7,11 +7,11 @@
 class MockTokenParser : public TokenParser
 {
 public:
-    MOCK_METHOD((void), do_start_call, (), (override));
-    MOCK_METHOD((void), do_end_call, (), (override));
-    MOCK_METHOD((void), do_string_call, (const std::string &), (override));
-    MOCK_METHOD((void), do_digit_call, (const std::string &), (override));
-    MOCK_METHOD((void), do_token_call, (const std::string &), (override));
+    MOCK_METHOD(void, do_start_call, (), (override));
+    MOCK_METHOD(void, do_end_call, (), (override));
+    MOCK_METHOD(void, do_string_call, (const std::string &), (override));
+    MOCK_METHOD(void, do_digit_call, (const std::string &), (override));
+    MOCK_METHOD(void, do_token_call, (const std::string &), (override));
 };
 
 
@@ -21,17 +21,52 @@ public:
 
 };
 
-TEST_F(TestTokenParser, test_simple)
+TEST_F(TestTokenParser, test_gmock_test)
 {
     MockTokenParser parser;
+
     EXPECT_CALL(parser, do_start_call()).Times(1);
     EXPECT_CALL(parser, do_end_call()).Times(1);
     EXPECT_CALL(parser, do_string_call("text")).Times(1);
     EXPECT_CALL(parser, do_string_call("Some")).Times(1);
-    EXPECT_CALL(parser, do_token_call("text")).Times(1);;
+    EXPECT_CALL(parser, do_token_call("text")).Times(1);
     EXPECT_CALL(parser, do_token_call("Some")).Times(1);
 
     parser.parse("Some text");
+}
+
+TEST_F(TestTokenParser, test_empty)
+{
+    MockTokenParser parser;
+
+    EXPECT_CALL(parser, do_start_call()).Times(1);
+    EXPECT_CALL(parser, do_end_call()).Times(1);
+
+    parser.parse("");
+}
+
+TEST_F(TestTokenParser, test_one_string)
+{
+    MockTokenParser parser;
+
+    EXPECT_CALL(parser, do_start_call()).Times(1);
+    EXPECT_CALL(parser, do_end_call()).Times(1);
+    EXPECT_CALL(parser, do_token_call("String")).Times(1);
+    EXPECT_CALL(parser, do_string_call("String")).Times(1);
+
+    parser.parse("String");
+}
+
+TEST_F(TestTokenParser, test_one_digit)
+{
+    MockTokenParser parser;
+
+    EXPECT_CALL(parser, do_start_call()).Times(1);
+    EXPECT_CALL(parser, do_end_call()).Times(1);
+    EXPECT_CALL(parser, do_token_call("123")).Times(1);
+    EXPECT_CALL(parser, do_digit_call("123")).Times(1);
+
+    parser.parse("123");
 }
 
 
