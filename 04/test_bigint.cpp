fa57e816930b7176537899c495e73d3286a5ae27
LeoQay <reliativism@yandex.ru>
2022-04-18 22:36:32 +0300

    04 reformat testing

--- 04/test_bigint.cpp
+++ 04/test_bigint.cpp
@@ -12,7 +12,18 @@ public:
 };
 
 
-class TestBigIntBase : public TestBigInt
+class TestBigIntInit : public TestBigInt {};
+
+
+class TestBigIntInitNoArgs : public TestBigIntInit {};
+
+class TestBigIntInitInt : public TestBigIntInit {};
+
+class TestBigIntInitStr : public TestBigIntInit {};
+
+class TestBigIntInitCopy : public TestBigIntInit {};
+
+class TestBigIntInitMove : public TestBigIntInit
 {
 public:
     static BigInt for_test_move_1();
@@ -22,7 +33,6 @@ public:
     static BigInt for_test_move_5();
     static BigInt for_test_move_6();
 };
-
 
 class TestBigIntEQ : public TestBigInt {};
 
@@ -36,121 +46,207 @@ void TestBigInt::cmp(const BigInt & obj, const std::string & s)
 }
 
 
-TEST_F(TestBigIntBase, test_init_no_args)
+TEST_F(TestBigIntInitNoArgs, test_init_no_args_1)
 {
     BigInt num;
     cmp(num, "0");
 }
 
 
-TEST_F(TestBigIntBase, test_init_int)
+TEST_F(TestBigIntInitInt, test_init_int_1)
 {
     BigInt num1(2);
     cmp(num1, "2");
+}
+
+TEST_F(TestBigIntInitInt, test_init_int_2)
+{
     BigInt num2(45);
     cmp(num2, "45");
+}
+
+TEST_F(TestBigIntInitInt, test_init_int_3)
+{
     BigInt num3(-12);
     cmp(num3, "-12");
+}
+
+TEST_F(TestBigIntInitInt, test_init_int_4)
+{
     BigInt num4(0);
     cmp(num4, "0");
+}
+
+TEST_F(TestBigIntInitInt, test_init_int_5)
+{
     BigInt num5(2147483647);
     cmp(num5, "2147483647");
+}
+
+TEST_F(TestBigIntInitInt, test_init_int_6)
+{
     BigInt num6(-2147483648);
     cmp(num6, "-2147483648");
 }
 
 
-TEST_F(TestBigIntBase, test_init_string)
+TEST_F(TestBigIntInitStr, test_init_str_1)
 {
     BigInt num1("2");
     cmp(num1, "2");
+}
+
+TEST_F(TestBigIntInitStr, test_init_str_2)
+{
     BigInt num2("23");
     cmp(num2, "23");
+}
+
+TEST_F(TestBigIntInitStr, test_init_str_3)
+{
     BigInt num3("124");
     cmp(num3, "124");
+}
+
+TEST_F(TestBigIntInitStr, test_init_str_4)
+{
     BigInt num4("-1234");
     cmp(num4, "-1234");
+}
+
+TEST_F(TestBigIntInitStr, test_init_str_5)
+{
     BigInt num5("0000");
     cmp(num5, "0");
+}
+
+TEST_F(TestBigIntInitStr, test_init_str_6)
+{
     BigInt num6("");
     cmp(num6, "0");
+}
+
+TEST_F(TestBigIntInitStr, test_init_str_7)
+{
     BigInt num7("4294967295");
     cmp(num7, "4294967295");
+}
+
+TEST_F(TestBigIntInitStr, test_init_str_8)
+{
     BigInt num8("4294967296");
     cmp(num8, "4294967296");
+}
+
+TEST_F(TestBigIntInitStr, test_init_str_9)
+{
     BigInt num9("-4294967296");
     cmp(num9, "-4294967296");
+}
+
+TEST_F(TestBigIntInitStr, test_init_str_10)
+{
     BigInt num10("-4294967297");
     cmp(num10, "-4294967297");
+}
+
+TEST_F(TestBigIntInitStr, test_init_str_11)
+{
     BigInt num11("12423345436356575674684835567468");
     cmp(num11, "12423345436356575674684835567468");
 }
 
 
-TEST_F(TestBigIntBase, test_copy)
+TEST_F(TestBigIntInitCopy, test_copy_1)
 {
     BigInt num1;
     BigInt num2(num1);
     cmp(num2, "0");
     cmp(num1, "0");
-    num1 = 234346;
+}
+
+TEST_F(TestBigIntInitCopy, test_copy_2)
+{
+    BigInt num1 = 234346;
     cmp(num1, "234346");
-    num2 = num1;
+    BigInt num2 = num1;
     cmp(num2, "234346");
     cmp(num1, "234346");
-    num1 = BigInt("2545847634758378365");
+}
+
+TEST_F(TestBigIntInitCopy, test_copy_3)
+{
+    BigInt num1 = BigInt("2545847634758378365");
     cmp(num1, "2545847634758378365");
-    num2 = num1;
+    BigInt num2 = num1;
     cmp(num1, "2545847634758378365");
     cmp(num2, "2545847634758378365");
-    num1 = -124124;
-    num2 = num1;
+}
+
+TEST_F(TestBigIntInitCopy, test_copy_4)
+{
+    BigInt num1 = -124124;
+    BigInt num2 = num1;
     cmp(num1, "-124124");
     cmp(num2, "-124124");
 
     num1 = BigInt("16545323254657");
-
     num1 = num1;
     num2 = num2;
-
     cmp(num1, "16545323254657");
     cmp(num2, "-124124");
 }
 
 
-BigInt TestBigIntBase::for_test_move_1()
+BigInt TestBigIntInitMove::for_test_move_1()
 {
     BigInt num = 1245;
     return num;
 }
 
-BigInt TestBigIntBase::for_test_move_2()
+TEST_F(TestBigIntInitMove, test_move_1)
+{
+    BigInt num;
+    num = for_test_move_1();
+    cmp(num, "1245");
+}
+
+BigInt TestBigIntInitMove::for_test_move_2()
 {
     BigInt num;
     return num;
 }
 
-BigInt TestBigIntBase::for_test_move_3()
+TEST_F(TestBigIntInitMove, test_move_2)
+{
+    BigInt num;
+    num = for_test_move_2();
+    cmp(num, "0");
+}
+
+BigInt TestBigIntInitMove::for_test_move_3()
 {
     BigInt num("-124235534235243");
     return num;
 }
 
-BigInt TestBigIntBase::for_test_move_4(BigInt & obj)
+TEST_F(TestBigIntInitMove, test_move_3)
+{
+    BigInt num;
+    num = for_test_move_3();
+    cmp(num, "-124235534235243");
+    num = for_test_move_4(num);
+    cmp(num, "-124235534235243");
+}
+
+BigInt TestBigIntInitMove::for_test_move_4(BigInt & obj)
 {
     return obj;
 }
 
-TEST_F(TestBigIntBase, test_move)
+TEST_F(TestBigIntInitMove, test_move_4)
 {
-    BigInt num;
-
-    num = for_test_move_1();
-    cmp(num, "1245");
-    num = for_test_move_2();
-    cmp(num, "0");
-    num = for_test_move_3();
-    cmp(num, "-124235534235243");
+    BigInt num("-124235534235243");
     num = for_test_move_4(num);
     cmp(num, "-124235534235243");
 }
