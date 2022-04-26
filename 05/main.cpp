995180246413f1a47aefcde3af60e64b620454f5
LeoQay <reliativism@yandex.ru>
2022-04-27 00:15:23 +0300

    05

--- 05/main.cpp
+++ 05/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 #include "serializer.hpp"
-
+#include "deserializer.hpp"
 
 
 struct Data
--- 05/test_serializer.cpp
+++ 05/test_serializer.cpp
@@ -2,30 +2,33 @@
 #include <gtest/gtest.h>
 
 #include "serializer.hpp"
+#include "deserializer.hpp"
+
+
+struct Data
+{
+    uint64_t a;
+    bool b;
+    uint64_t c;
+
+    template <class Serializer>
+    Error serialize(Serializer & serializer)
+    {
+        return serializer(a, b, c);
+    }
+
+    template<class Deserializer>
+    Error deserialize(Deserializer & deserializer)
+    {
+        return deserializer(a, b, c);
+    }
+};
+
 
 
 class TestSeries : public ::testing::Test
 {
 public:
-
-    struct Data
-    {
-        uint64_t a;
-        bool b;
-        uint64_t c;
-
-        template <class Serializer>
-        Error serialize(Serializer & serializer)
-        {
-            return serializer(a, b, c);
-        }
-
-        template<class Deserializer>
-        Error deserialize(Deserializer & deserializer)
-        {
-            return deserializer(a, b, c);
-        }
-    };
 };
 
 
