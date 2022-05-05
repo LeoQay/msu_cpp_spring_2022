2ef688496190f0288653092b9d051d48831f3ae3
LeoQay <reliativism@yandex.ru>
2022-05-06 01:16:10 +0300

    06 init

---
+++ 06/format.hpp
@@ -0,0 +1,16 @@
+#ifndef HW_CPP_VK_FORMAT_H
+#define HW_CPP_VK_FORMAT_H
+
+
+#include <string>
+#include <sstream>
+
+
+template<class... ArgsT>
+std::string format(const std::string & str, ArgsT ...args)
+{
+
+}
+
+
+#endif //HW_CPP_VK_FORMAT_H
---
+++ 06/test_format.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+
+#include "format.hpp"
+
+
+class TestFormat : public ::testing::Test {};
+
+
+TEST_F(TestFormat, test_1)
+{
+
+}
+
+
+int main()
+{
+    ::testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
+}
