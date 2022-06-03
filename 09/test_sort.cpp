2563f4ac54582b26dfcd3c167399adb9b3f8be0c
LeoQay <reliativism@yandex.ru>
2022-06-03 14:46:08 +0300

    prev some commits actually 08, not 09 09 test update

--- 09/test_sort.cpp
+++ 09/test_sort.cpp
@@ -8,14 +8,42 @@
 class TestSort : public ::testing::Test {};
 
 
-TEST_F(TestSort, test_very_simple)
+TEST_F(TestSort, test_very_small_size)
+{
+    Generator::gen("file.bin", 10);
+
+    TwoThreadSort sorter;
+    sorter.sort("file.bin", "result.bin");
+
+    ASSERT_TRUE(SortChecker::check("result.bin"));
+
+    std::filesystem::remove("file.bin");
+    std::filesystem::remove("result.bin");
+}
+
+
+TEST_F(TestSort, test_small_size)
+{
+    Generator::gen("file.bin", 1000);
+
+    TwoThreadSort sorter;
+    sorter.sort("file.bin", "result.bin");
+
+    ASSERT_TRUE(SortChecker::check("result.bin"));
+
+    std::filesystem::remove("file.bin");
+    std::filesystem::remove("result.bin");
+}
+
+
+TEST_F(TestSort, test_big_size)
 {
     Generator::gen("file.bin", 10000000);
 
     TwoThreadSort sorter;
     sorter.sort("file.bin", "result.bin");
 
-    std::cout << SortChecker::check("result.bin");
+    ASSERT_TRUE(SortChecker::check("result.bin"));
 
     std::filesystem::remove("file.bin");
     std::filesystem::remove("result.bin");
